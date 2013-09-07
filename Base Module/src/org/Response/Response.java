834bf30776a9a66b816ad6f633628c731dbbc586
spancha <spancha@ncsu.edu>
2013-09-07 13:11:22 -0400

    recommit response

--- Base Module/src/org/Response/Response.java
+++ Base Module/src/org/Response/Response.java
@@ -1,5 +1,30 @@
 package org.Response;
 
+import java.util.HashMap;
+
+import org.common.ResponseType;
+
 public class Response {
+	
+	ResponseType type;
+	HashMap<String, Object> data;	
+	public Response()
+	{
+		data = new HashMap<>();
+	}
+	public ResponseType getType() {
+		return type;
+	}
+	public void setType(ResponseType type) {
+		this.type = type;
+	}
+	public HashMap<String, Object> getData() {
+		return data;
+	}
+	public void setData(HashMap<String, Object> data) {
+		this.data = data;
+	}
+	
 
 }
+
