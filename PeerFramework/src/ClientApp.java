43a731abed9b9f61b76c8d16350f77cf4edf52af
riturajsacademic <rituraj.s.academic@gmail.com>
2013-09-14 19:48:07 -0400

    Modular Function + Main Class
    the main class + the function implementation of request and response.

---
+++ PeerFramework/src/ClientApp.java
@@ -0,0 +1,14 @@
+import org.common.DataKeyConstants;
+
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientFunction peer = new ClientFunction();
+		peer.registerPeer();
+		
+		//peer.leaveFunc();
+	}
+
+}
