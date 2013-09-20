c6fada0398e7eca053ebf3459dd1e3851de939f3
spancha <spancha@ncsu.edu>
2013-09-19 20:35:48 -0400

    added new keys

--- Base Module/src/org/common/DataKeyConstants.java
+++ Base Module/src/org/common/DataKeyConstants.java
@@ -8,4 +8,8 @@ public class DataKeyConstants {
 	public static final String RFC_INDEX = "rfcindex";//RFC which peer downloads.
 	public static final String ERROR_MESSAGE = "error message";
 	public static final String PEER_LIST = "peerList";
+	public static final String RFC_INDEX_LIST = "rfcIndexList";
+	public static final String RFC_NUMBER ="rfcNumber";
+	public static final String RFC_TITLE ="rfcTitle";
+	public static final String RFC_FILE_DATA = "rfcFileData";
 }
