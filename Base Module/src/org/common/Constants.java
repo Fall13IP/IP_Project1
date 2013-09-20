40eadb6a663eeecc7cce3e70443f42b4d6e4ac21
spancha <spancha@ncsu.edu>
2013-09-19 20:33:54 -0400

    added new error messages

--- Base Module/src/org/common/Constants.java
+++ Base Module/src/org/common/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
 	public final static int INITIAL_TTL_VALUE = 7200;
 	public final static String INVALID_COOKIE = "Invalid cookie";
 	public final static String PEER_NOT_FOUND = " Peer not found on RS server";
-	public final static String SERVER_IP_ADDRESS="10.139.62.151";
-	public final static String SERVER_PORT_ADDRESS = "1000";
+	public final static String SERVER_IP_ADDRESS="192.168.2.14";
+	
+	public final static int PEER_SERVER_PORT_NUMBER = 2000;
+	public final static String RFC_NOT_FOUND = "Requested RFC not found";
 }
