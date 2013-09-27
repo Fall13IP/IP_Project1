ad7ea7b6b74c0176444f5bf865a303bfabbabacb
spancha <spancha@ncsu.edu>
2013-09-26 23:25:23 -0400

    updating

--- Task1/src/org/peer/Peer.java
+++ Task1/src/org/peer/Peer.java
@@ -1,5 +1,9 @@
 package org.peer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
@@ -15,7 +19,7 @@ import org.request.RequestHelper;
 public class Peer {
 	
 	private static ClientFunction peer;
-
+	private static String peerName;
 	public static void startPeer(int serverPortNumber){		
 		peer.registerPeer(serverPortNumber);
 		peer.pQueryFunc();
@@ -67,17 +71,24 @@ public class Peer {
 		startPeerTask2(serverPortNumber);
 	}
 	
+
 	public static void startTransferTask2(int serverPortNumber){
 		
+		BufferedWriter bufferedWriter;
+		
+		try {
+			 bufferedWriter = new BufferedWriter(new FileWriter("Timing_" + peerName + ".txt"));
+	
+		
 		long[] TimeTracker;
-		TimeTracker = new long[50];
+		
 		long cumlativeTime = 0;
 		List <PeerListNode> peerList;
 		peer.pQueryFunc();
 		peerList = peer.getPeerList();
 		//PeerListNode node = peerList.get(rand(peerList.size()));
 		for(int i=0; i < peerList.size(); i++){
-			PeerListNode node = peerList.get(0);
+			PeerListNode node = peerList.get(i);
 			if(node.getHostName().equals(RequestHelper.getIP()) && node.getPortNumber() == serverPortNumber){
 				
 			}else{
@@ -85,9 +96,15 @@ public class Peer {
 			}
 		}
 		
+		System.out.println("Got RFC index from all peers, type any number to start transfer");
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		scanner.close();
 		List<RFCIndexNode> rfcList = ClientFunction.getRfcIndexList();
+		TimeTracker = new long[rfcList.size()];
 		System.out.println("RFC Count:"+rfcList.size());
 		Calendar c1 = null,c2=null;
+		long accTime = 0;
 		for(int RFCCount=0;RFCCount<rfcList.size();RFCCount++)
 		{	
 				
@@ -95,7 +112,8 @@ public class Peer {
 			String rfcTitle = rfcList.get(RFCCount).getRfcTitle();
 			int rfcNo = rfcList.get(RFCCount).getRfcNumber();
 			System.out.println("RFC title " + rfcTitle + "  rfc no " + rfcNo);
-			if(rfcNode.getRfcHostname() != RequestHelper.getIP()){
+			if(!(rfcNode.getRfcHostname().equals(RequestHelper.getIP()))){
+				System.out.println("rfcNode.getRfcHostname() " + rfcNode.getRfcHostname());
 				PeerListNode node = getPeer(rfcNode.getRfcHostname(), peerList);
 				c1=Calendar.getInstance();	
 				byte [] fileData = peer.GetRFCFunc(node, rfcNo, rfcTitle);
@@ -106,7 +124,13 @@ public class Peer {
 				c2=Calendar.getInstance();
 				
 				TimeTracker[RFCCount]=c2.getTimeInMillis()-c1.getTimeInMillis();
-				System.out.println("RFC no in array: "+RFCCount+" time: "+(TimeTracker[RFCCount]));
+				accTime = accTime + TimeTracker[RFCCount];
+				String printValue = "RFC no in array: "+RFCCount+" time: "+(TimeTracker[RFCCount]);
+				System.out.println(printValue);
+				bufferedWriter.append(printValue);
+				bufferedWriter.newLine();
+				bufferedWriter.append(String.valueOf(accTime));
+				bufferedWriter.newLine();
 			}
 			
 		
@@ -117,7 +141,108 @@ public class Peer {
 			cumlativeTime= cumlativeTime+TimeTracker[timeIterator];
 		}
 		System.out.println("Cumulative time: " + cumlativeTime);
+		bufferedWriter.close();
+	} catch (IOException e) {
+			
+			e.printStackTrace();
+	}
 		
+	}
+	
+	private static void startTransferBestCase(int serverPortNumber){
+		
+		BufferedWriter bufferedWriter;
+		
+		try {
+			 bufferedWriter = new BufferedWriter(new FileWriter("Timing_best" + peerName + ".txt"));
+	
+		
+		long[] TimeTracker;
+		
+		long cumlativeTime = 0;
+		List <PeerListNode> peerList;		
+		peerList = peer.getPeerList();
+		//PeerListNode node = peerList.get(rand(peerList.size()));		
+		
+		List<RFCIndexNode> rfcList = ClientFunction.getRfcIndexList();
+		TimeTracker = new long[rfcList.size()];
+		System.out.println("RFC Count:"+rfcList.size());
+		Calendar c1 = null,c2=null;
+		long accTime = 0;
+		for(int RFCCount=0;RFCCount<rfcList.size();RFCCount++)
+		{	
+				
+			RFCIndexNode rfcNode = rfcList.get(RFCCount);
+			String rfcTitle = rfcList.get(RFCCount).getRfcTitle();
+			int rfcNo = rfcList.get(RFCCount).getRfcNumber();
+			System.out.println("RFC title " + rfcTitle + "  rfc no " + rfcNo);
+			if(!(rfcNode.getRfcHostname().equals(RequestHelper.getIP()))){
+				System.out.println("rfcNode.getRfcHostname() " + rfcNode.getRfcHostname());
+				PeerListNode node = getPeer(rfcNode.getRfcHostname(), peerList);
+				c1=Calendar.getInstance();	
+				byte [] fileData = peer.GetRFCFunc(node, rfcNo, rfcTitle);
+				if(fileData != null){
+					System.out.println("File size: " + fileData.length);
+					//ClientHelper.writeToDisk(fileData, rfcTitle);
+				}
+				c2=Calendar.getInstance();
+				
+				TimeTracker[RFCCount]=c2.getTimeInMillis()-c1.getTimeInMillis();
+				accTime = accTime + TimeTracker[RFCCount];
+				String printValue = "RFC no in array: "+RFCCount+" time: "+(TimeTracker[RFCCount]);
+				System.out.println(printValue);
+				bufferedWriter.append(printValue);
+				bufferedWriter.newLine();
+				bufferedWriter.append(String.valueOf(accTime));
+				bufferedWriter.newLine();
+			}
+			
+		
+		}
+		
+		
+		bufferedWriter.close();
+	} catch (IOException e) {
+			
+			e.printStackTrace();
+	}
+		
+	}
+	
+	private static void bestCase(int serverPortNumber){
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true){		
+		
+			System.out.println("Enter 1 to register");
+			System.out.println("Enter 2 to Pquery");
+			System.out.println("Enter 3 to get rfc index");
+			System.out.println("Enter 4 to begin transfer");
+			int input= scanner.nextInt();
+			switch(input){
+			case 1: 
+				Peer.startPeerTask2(serverPortNumber);
+				break;
+			case 2: {
+				peer.pQueryFunc();
+				break;
+			}
+				
+			case 3:{
+				System.out.println("Enter index in peerList to get rfc index from");
+				int peerNo = scanner.nextInt();
+				List<PeerListNode> peerList = peer.getPeerList();
+				PeerListNode peerNode = peerList.get(peerNo);
+				peer.RFCIndexFunc(peerNode);
+				break;
+			}
+			case 4:{
+				startTransferBestCase(serverPortNumber);
+				break;
+			}
+				
+			}
+		}
 	}
 	
 	private static PeerListNode getPeer(String ip, List<PeerListNode> peerList){
@@ -125,7 +250,7 @@ public class Peer {
 		PeerListNode node = null;
 		for(int i=0; i < peerList.size(); i ++){
 			node = peerList.get(i);
-			if(node.getHostName() == ip){
+			if(node.getHostName().equals(ip)){
 				return node;
 			}
 		}
@@ -159,25 +284,29 @@ public class Peer {
 			System.out.println("Enter 3 to leave P2P");
 			int input= scanner.nextInt();
 			switch(input){
-			case 1: Peer.task2(serverPortNumber);
-				break;
-			case 2: Peer.startTransferTask2(serverPortNumber);;
-				break;
-			case 3: peer.leaveFunc();
-				break;
+			case 1: 
+				Peer.task2(serverPortNumber);
+					break;
+			case 2: 
+				Peer.startTransferTask2(serverPortNumber);
+					break;
+			case 3: 
+				peer.leaveFunc();
+					break;
 			}
 		}
 	}
 	public static void main(String[] args) {
-		if (args.length == 3){
+		if (args.length == 4){
 			String confFileName = args[0];
 			int serverPortNumber = Integer.valueOf(args[1]);
 			String rsServerIP = args[2];
+			peerName = args[3];
 			peer = new ClientFunction(confFileName,rsServerIP);		
 			PeerServerApp peerServerApp = new PeerServerApp(serverPortNumber);
 			peerServerApp.start();
 			System.out.println("Peer server started");
-			System.out.println("Enter 1 for TestCase1 otherwise 2 for TestCase2");
+			System.out.println("Enter 1 for TestCase1 , 2 for TestCase2, 3 for bestcase");
 			Scanner scanner = new Scanner(System.in);
 			while(true){
 				int UserChoice= scanner.nextInt();
@@ -185,9 +314,12 @@ public class Peer {
 				{
 					startTest1(serverPortNumber);
 				}
-				else
+				else if(UserChoice == 2)
 				{
 					startTest2(serverPortNumber);
+				}
+				else if(UserChoice == 3){
+					bestCase(serverPortNumber);
 				}
 			}
 			
